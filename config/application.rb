acb5dafe8a9c680b5d950e517f39594cc6a72439
eman <e2masterdev@gmail.com>
2016-10-29 18:51:34 +0300

    attempt1

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,11 @@
 require_relative 'boot'
 require 'certified'
-#require 'rails/all'
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-require "action_cable"
-require "rails/test_unit/railtie"
+require 'rails/all'
+# require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "sprockets/railtie"
+# require "action_cable"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
