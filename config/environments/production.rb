1abde1e6940faf26922f52f8d7f87c4980f913de
eman <e2masterdev@gmail.com>
2016-10-29 19:26:31 +0300

    action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'https://e2masterdev-playground.herokuapp.com/', port: 443 }
+  config.action_mailer.default_url_options = { host: 'e2masterdev-playground.herokuapp.com/', port: 443 }
   config.action_mailer.delivery_method = :smtp
   # Settings specified here will take precedence over those in config/application.rb.
 
